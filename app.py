d8a4bf4c8eacef054f54f5a0d436315eff2e0379
vera-wang-14 <314verawang@gmail.com>
2025-01-15 08:26:53 +0000

    1

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 from flask import Flask
 from flask import render_template, request
+
+app = Flask(__name__)
+
+@app.route("/") # old style's first line to run
+
+def index(): 
+    return(render_template("index.html"))
+#do nothing but render template
+
+if __name__ == "__main__":
+    app.run()
+
