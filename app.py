fa0dd5e694220e813cc59b1fff9efa8b06145b34
vera-wang-14 <314verawang@gmail.com>
2025-04-02 08:51:18 +0000

    1

--- app.py
+++ app.py
@@ -31,6 +31,10 @@ def sentiment_result():
     r = textblob.TextBlob(q).sentiment
     return(render_template("sentiment_result.html", r = r))
 
+@app.route("/paynow", methods=["GET", "POST"])
+def paynow():
+    return(render_template("paynow.html"))
+
 if __name__ == "__main__":
     app.run(port=1234)
 
